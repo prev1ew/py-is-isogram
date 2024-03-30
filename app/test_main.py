5d152ff42eb6aafd07edefb482aa06007812fc86
Vitalii <warcied1@gmail.com>
2024-03-30 11:38:27 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "param_in, expected_result",
+    [
+        ("", True),
+        ("a", True),
+        ("Aa", False),
+        ("abra", False),
+        ("abrA", False),
+        ("abcdefj", True),
+    ]
+)
+def test_correct_values(param_in: str, expected_result: bool) -> None:
+    assert is_isogram(param_in) == expected_result
